9cde3a3867ccf23ce2ad6d70a92d694d0fa4bac3
HanneWhitt <hannes.whittingham@gmail.com>
2025-01-13 16:32:51 +0000

    Task.py script name changed

--- MATH_Dataset_Task.py
+++ MATH_Dataset_Task.py
@@ -1,18 +1,23 @@
 import re
 import pandas as pd
-from Task import Task
+from base_task import Task
 
 
 class MATH_Dataset_Task(Task):
 
+    # By default, set the name as the class name
+    name = 'MATH_Dataset_Task'
+
     def __init__(
         self,
+        language_aspect,
         dataset='MATH_dataset/MATH_dataset_minimal.csv',
         shuffle=True
     ):
+        super().__init__(language_aspect=language_aspect)
+        
         self.dataset = pd.read_csv(dataset)
         self.length = len(self.dataset)
-        self.answer_pattern = re.compile('boxed{(.+)}')
         self.problem_index_pattern = re.compile('Problem ([0-9]+)')
         self.shuffle = shuffle
 
@@ -29,7 +34,9 @@ class MATH_Dataset_Task(Task):
 
     def get_task_score(
         self,
-        transcript
+        prompt,
+        final_answer,
+        info
     ):
         # Get problem index from transcript
         idx = int(re.findall(self.problem_index_pattern, transcript)[0])
@@ -41,25 +48,7 @@ class MATH_Dataset_Task(Task):
         return float(true_answer == llm_answer)
         
 
-    def get_language_score(
-        self,
-        transcript
-    ):
-        # Cut transcript down to just CoT by:
-        # Removing prompt
-        cot_start_idx = transcript.find(self.cot_instruction) + len(self.cot_instruction) + 1
-        cot = transcript[cot_start_idx:]
-        # Removing final answer
-        for ans in re.findall(self.answer_pattern, transcript):
-            cot = cot.replace(ans, '')
-        # Count equals signs in cot
-        n_equals_signs = cot.count('=')
-        # We want to drive down the number of equals signs
-        # We will punish based on use up to max of 50 times
-        SIGN_MAX = 50
-        n_equals_signs = min(n_equals_signs, SIGN_MAX)
-        language_score = float(n_equals_signs/SIGN_MAX)
-        return language_score
+
 
 
 if __name__ == '__main__':
@@ -81,7 +70,3 @@ if __name__ == '__main__':
     reward = math_task.reward_from_transcript(transcript)
 
     print(reward)
-
-
-
-
