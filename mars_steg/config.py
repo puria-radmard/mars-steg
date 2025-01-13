576dccb91296a2d75b24b7431b38bb1c39de28db
Connor Watts <connorwatts@hotmail.com>
2025-01-13 15:23:54 +0000

    Model Loader/Config

--- mars_steg/config.py
+++ mars_steg/config.py
@@ -1,1 +1,19 @@
-# config classes (store jsons for runs somewhere else)
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class ModelConfig:
+    model_name: str
+    lora: bool = False
+    lora_r: int = 16
+    lora_alpha: int = 32
+    lora_dropout: float = 0.1
+    lora_bias: str = "none"
+
+    @property
+    def lora_params(self):
+        return {
+            "r": self.lora_r,
+            "lora_alpha": self.lora_alpha,
+            "lora_dropout": self.lora_dropout,
+            "bias": self.lora_bias
+        }
--- mars_steg/model/loader.py
+++ mars_steg/model/loader.py
@@ -1,1 +1,37 @@
-# file for loading models
+from mars_steg.config import ModelConfig
+from peft import LoraConfig, get_peft_model, TaskType
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+)
+
+def get_model(model_config: ModelConfig, tokenizer: AutoTokenizer):
+    """Get a model."""
+    model = AutoModelForCausalLM.from_pretrained(model_config.model_name)
+    model.resize_token_embeddings(len(tokenizer))
+    if model_config.lora:
+        lora_config = LoraConfig(
+            task_type=TaskType.CAUSAL_LM,
+            **model_config.lora_params
+        )
+        model = get_peft_model(model, lora_config)
+        
+        model.print_trainable_parameters()
+    return model
+
+
+if __name__ == '__main__':
+
+    model_config = ModelConfig(
+        model_name="gpt2",
+        lora=True,
+        lora_r=16,
+        lora_alpha=32,
+        lora_dropout=0.1,
+        lora_bias="none"
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_config.model_name)
+    model = get_model(model_config, tokenizer)
+    print(model)
+    
