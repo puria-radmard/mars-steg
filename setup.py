e86abd0891fd7c58a4cf80d4e62855eb491fca92
Mustafa Yasir WorkMacOS <muzz_yasir@hotmail.com>
2025-01-14 18:02:25 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mars-steg",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+        # You can copy these from requirements.txt
+    ],
+    author="mars-steg",
+    description="Mars Steg project",
+    python_requires=">=3.6",
+)
