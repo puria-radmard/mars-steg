0b9f0a9227f44f718ec559cfb78d9c80e57a6138
Mustafa Yasir WorkMacOS <muzz_yasir@hotmail.com>
2025-01-30 00:15:24 +0200

    integrating price game with new prompt configs

--- run_game.sh
+++ run_game.sh
@@ -1,2 +1,4 @@
 #/bin/bash
-python -m mars_steg.train_dataset_task experiments/b_sequential_price_game/1_price_mention.yaml mars_steg/configs/config_llama_1b.yaml
+python -m mars_steg.train_dataset_task experiments/b_sequential_price_game/1_collusion_with_neural_overseer.yaml mars_steg/configs/config_llama_1b.yaml mars_steg/configs/prompt/price_training_llama.yaml
+
+
