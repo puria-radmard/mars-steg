1d7a29d3514ce7b198e6ef6f2a3f7fd93b3c9003
HanneWhitt <hannes.whittingham@gmail.com>
2025-01-30 15:59:34 +0000

    added docs for score.py

--- mars_steg/utils/score.py
+++ mars_steg/utils/score.py
@@ -9,8 +9,27 @@ import torch as t
 from dataclasses import dataclass
 
 
-# Type checking [0,1] scores
-def check_score(score_name, score_value):
+def check_score(
+    score_name: str,
+    score_value: float
+):
+
+    """
+    Checks that a given score can be cast to a float in the range [0, 1], and returns that float if so. 
+
+    Returns
+    -------
+    float
+        The score, cast to a float
+
+    Raises
+    ------
+    TypeError
+        If the score is not an appropriate type (i.e float, bool, int)
+    ValueError
+        If the score is an appropriate type but outside range [0, 1]
+    """
+
     if not isinstance(score_value, (float, bool, int)):
         raise TypeError(f'.get_{score_name}() must bool, or float in range [0,1], or ints 0 or 1. Returned type: {type(score_value)}')
     score_value = float(score_value)
@@ -21,6 +40,12 @@ def check_score(score_name, score_value):
 
 @dataclass
 class CoTGapSummary:
+
+    """
+    A dataclass to hold a summary of task performance with and without use of a CoT.
+
+    """
+
     sum_examples: int
     sum_extracted_with_cot: int
     sum_extracted_without_cot: int
