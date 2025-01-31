db668c1ddf315e41836db9b727a83eb838caf64c
Joey SKAF <jskaf@Joeys-MacBook-Pro.local>
2025-01-31 10:41:21 +0100

    rebasing documentation into main, with a small conflict that has been handled

--- mars_steg/task/neural_assessor.py
+++ mars_steg/task/neural_assessor.py
@@ -65,7 +65,7 @@ class ProblemSolutionAssessor:
 
 
     """
-    
+
     def __init__(
         self, 
         model: BaseModel,
@@ -79,6 +79,7 @@ class ProblemSolutionAssessor:
         # TODO: move to config
         self.user_prompt = prompt_config.neural_assessor_user_prompt_template
         self.system_prompt = prompt_config.neural_assessor_system_prompt
+
 
     def convert_batch_prompt_into_conversation_template(
         self,
@@ -114,15 +115,12 @@ class ProblemSolutionAssessor:
             assessor_prompts.append(
                 self.user_prompt.format(
                     # agent_prompt = pd.cot_prompt if with_cot else pd.no_cot_prompt,
-                    llm_answer = pd.extracted_final_answer_with_cot if with_cot else pd.extracted_final_answer_without_cot, 
-                    true_answer = ta
+                    llm_answer=pd.extracted_final_answer_with_cot if with_cot else pd.extracted_final_answer_without_cot, 
+                    true_answer=ta
                 )
             )
 
         return self.whitebox_model.batchize_conversation(assessor_prompts, self.system_prompt)
-
-
-        return self.whitebox_model.batchize_conversation(self.system_prompt, assessor_prompts)
 
     @torch.no_grad()
     def get_assessor_generated_answers(
@@ -133,7 +131,7 @@ class ProblemSolutionAssessor:
         neural_assessor_thinking_helper: str = None
     ) -> BatchPromptData:
         """
-        Generates assessor transcripts and assigns it to a new BatchPromptData object (cf. notes for some important information about unsuccessful examples).
+        Generates assessor transcripts and assigns it to a new BatchPromptData object (see notes for some important information about unsuccessful examples).
 
         This function processes a BatchPromptData (list of PromptData objects) through the neural assessor model, 
         generating transcripts that evaluate the correctness of model-generated answers. 
