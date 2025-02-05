bf257b3f1e4fbf804acd7e3845758940e01de042
P. Radmard <pr450@mirzakhani.eng.cam.ac.uk>
2025-02-05 23:25:19 +0000

    removed unneeded sh

--- scripts/run_pricing_game_first_test.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-python -m \
-    mars_steg.train_dataset_task \
-    experiments/b_sequential_price_game/1_collusion_with_neural_overseer.yaml \
-    mars_steg/configs/config_llama_8b_no_evals.yaml \
-    mars_steg/configs/prompt/price_training_llama.yaml
