7f0f4bcfeadec26752a1835c46e7cbe93958270f
Luisibear98 <43785734+Luisibear98@users.noreply.github.com>
2025-02-07 18:46:05 +0100

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-python3.11 -m pip install --upgrade pip
-python3.11 -m pip install -r requirements.txt
+apt-get update && apt-get install -y tmux
+apt-get install -y tmux
+tmux new -s mysession
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 echo "Setup completed successfully."
 chmod +x ./scripts/run_pricing_game_first_runpod_test.sh
