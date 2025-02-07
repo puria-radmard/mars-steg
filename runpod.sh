bca49848112e272f69df0c713d5c2bf3be1937c5
P. Radmard <pr450@mirzakhani.eng.cam.ac.uk>
2025-02-07 14:34:06 +0000

    update to dockerfile

--- runpod.sh
+++ runpod.sh
@@ -2,9 +2,4 @@
 python3.11 -m pip install --upgrade pip
 python3.11 -m pip install -r mars-steg/requirements.txt
 
-
-HF_TOKEN=
-python3.11 -c "from huggingface_hub import HfFolder; HfFolder.save_token('$HF_TOKEN')"
-
 echo "Setup completed successfully."
-
