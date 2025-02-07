306e3fc52e239670cd735916d474c047a9536cd1
Luisibear98 <43785734+Luisibear98@users.noreply.github.com>
2025-02-07 20:39:45 +0100

    Update and rename runpod.sh to tmux_install

--- runpod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y tmux
-tmux new -s mysession
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-echo "Setup completed successfully."
-chmod +x ./scripts/run_pricing_game_first_runpod_test.sh
-./scripts/run_pricing_game_first_runpod_test.sh
