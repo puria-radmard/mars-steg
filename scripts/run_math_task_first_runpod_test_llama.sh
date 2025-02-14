0d06b0fca1da4470a44c5df9c0a8435513b7801e
Joey SKAF <jskaf@Joeys-MacBook-Pro.local>
2025-02-14 18:24:17 +0100

    updated config for math task

---
+++ scripts/run_math_task_first_runpod_test_llama.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+python -m \
+    mars_steg.train_dataset_task \
+    experiments/a_math/1_equals_mention_with_neural_assessor.yaml \
+    mars_steg/configs/config_llama_1b_no_evals.yaml \
+    mars_steg/configs/prompt/math_training_llama.yaml  # XXX: 20250212
