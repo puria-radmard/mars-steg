e090779c4aa2b1cd2ecee745870f576772acc780
Joey SKAF <jskaf@Joeys-MacBook-Pro.local>
2025-02-26 13:11:08 +0100

    Script modified

--- scripts/accelerate_scripts/deepseek_7b_price_game.sh
+++ scripts/accelerate_scripts/deepseek_7b_price_game.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICE="0,1,2" ACCELERATE_LOG_LEVEL=info accelerate launch --config_file mars_steg/configs/accelerate/deepspeed_zero3.yaml mars_steg.accelerate_training \
+CUDA_VISIBLE_DEVICE="0,1,2" ACCELERATE_LOG_LEVEL=info accelerate launch --config_file mars_steg/configs/accelerate/deepspeed_zero3.yaml mars_steg/accelerate_training.py \
     experiments/b_sequential_price_game/1_collusion_with_neural_overseer.yaml \
     mars_steg/configs/config_deepseek_7b.yaml \
     scripts/1_testing_with_and_without_penalisation/with_penalise_overwrite.yaml \
